9f82f7566f5b1ddefce441173f42e361342422cc
promisecaring <poghene@byupathway.edu>
2026-05-23 19:44:47 +0100

    scripture

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,7 +7,7 @@ class Program
         Reference reference = new Reference("Proverbs", 3, 5, 6);
 
         string text =
-            "Trust in the Lord with all your heart and lean not on your own understanding";
+            "Trust in thy Lord with all your heart and lean not on your own understanding";
 
         Scripture scripture = new Scripture(reference, text);
 
